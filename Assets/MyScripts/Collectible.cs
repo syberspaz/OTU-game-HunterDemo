ae12c80878082f19eb867054f5e900da4ce32593
syberspaz <syberspaz@gmail.com>
2022-05-09 08:40:03 -0400

    fun vfx
    playing with fun vfx

--- Assets/MyScripts/Collectible.cs
+++ Assets/MyScripts/Collectible.cs
@@ -6,6 +6,22 @@ public class Collectible : MonoBehaviour
 {
     public bool isOn;
     public UnityAction<Collectible> OnPickup;
+    public GameObject energyPrefab;
+    private Material energyMat;
+    float life;
+    public void Start()
+    {
+        energyPrefab = Instantiate(energyPrefab, gameObject.transform, false);
+        energyPrefab.SetActive(true);
+        MeshRenderer m = energyPrefab.GetComponentInChildren<MeshRenderer>();
+        Debug.Log(m);
+        energyMat = m.material;
+        Debug.Log(energyMat);
+        energyMat.SetFloat("LIFE", 0.0f);
+        energyPrefab.transform.SetParent(gameObject.transform);
+        energyPrefab.transform.localScale = Vector3.zero;
+        life = 0.0f;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag != "Player") return;
@@ -16,12 +32,19 @@ public class Collectible : MonoBehaviour
                 .setEaseOutBounce()
                 .setOnComplete(AnimationComplete);
             //LeanTween.scale(gameObject, gameObject.transform.localScale * 1.5f, 0.5f)
-              //  .setEaseOutBounce()
-                
-
+            //  .setEaseOutBounce()
+            life = 1.0f;
+            energyMat.SetFloat("LIFE", 1.0f);
+            LeanTween.value(life, 0.0f, 1.2f).setEaseOutQuart().setOnUpdate(updateMaterial);
+            LeanTween.scale(energyPrefab, Vector3.one*5.0f, 0.5f);
         }
         //yield return new WaitUntil(()=>!LeanTween.isTweening(gameObject));
         
+    }
+    private void updateMaterial(float val)
+    {
+        energyMat.SetFloat("LIFE", val);
+        //Debug.Log(val);
     }
     private void AnimationComplete()
     {
