9e2e9beb8fc5d4f55316796535bce462e5e28c93
syberspaz <syberspaz@gmail.com>
2022-05-11 10:15:07 -0400

    Update selectorAnimate.cs

--- Assets/selectorAnimate.cs
+++ Assets/selectorAnimate.cs
@@ -10,6 +10,7 @@ public class selectorAnimate : MonoBehaviour
         LeanTween.scale(gameObject, gameObject.transform.localScale*0.75f, 2.0f).setLoopPingPong();
         //LeanTween.rotateAround(gameObject, Vector3.forward, 360, 2.5f).setLoopClamp();
         LeanTween.rotateAround(gameObject, new Vector3(1,1,0), 360, 5.0f).setLoopClamp();
+        //LeanTween.
     }
 
     // Update is called once per frame
